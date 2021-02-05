0a469d65eba9dca99159880ef9fc20664a3cd76e
Lesetja Lekoloane <lekoloanelesetja@gmail.com>
2021-02-05 09:59:48 +0000

    added water and ETC

--- code.bash
+++ code.bash
@@ -3,6 +3,6 @@
 #This is git and github demo script
 
 echo A list of things you need to make coffee
-echo water. coffee beans. sugar. milk. mug.
+echo hot water. coffee beans. sugar. milk. mug. ETC..
 
 
